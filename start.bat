186f55f2fd8d7927e5c07451def8024c7c28187d
Ankit8544 <ankitkumar875740@gmail.com>
2023-09-13 14:52:03 +0530

    https://github.com/Ankit8544/EXCEL-API.git

---
+++ start.bat
@@ -0,0 +1,8 @@
+rem Set environment variables if needed
+rem set YOUR_ENV_VAR=value
+
+rem Activate your virtual environment (if you're using one)
+rem call C:\path\to\your\virtualenv\Scripts\activate
+
+rem Run your Flask app
+python Ans_01.py
